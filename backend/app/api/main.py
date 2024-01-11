d23848ce0f74fdfcfaa795d3c66e0f2858cd16d8
Natacha Bertin <32799433+natachabertin@users.noreply.github.com>
2024-01-11 11:58:46 -0300

    main: added dunder main to ide alternative debugging

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,8 +1,13 @@
 from backend.app.core.server import configure_server
 
+
 app = configure_server()
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    app
