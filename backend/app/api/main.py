b297c531e2e5f982caf537b0fd665d7c5d711a4d
Natacha Bertin <32799433+natachabertin@users.noreply.github.com>
2024-01-11 11:57:12 -0300

    server: refactored out of main

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,21 +1,6 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from backend.app.core.server import configure_server
 
-app = FastAPI()
-
-ALLOWED_ORIGINS = [
-    "http://127.0.0.1:8000",
-    "http://0.0.0.0:8000",
-    "http://localhost:8000",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=ALLOWED_ORIGINS,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+app = configure_server()
 
 
 @app.get("/")
