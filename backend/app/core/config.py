c57d5aed1f33880b5a07a3bb367190c7033f93e4
Natacha Bertin <32799433+natachabertin@users.noreply.github.com>
2024-01-14 14:53:11 -0300

    main/config: settings updated

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,8 +2,18 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    app_name: str = "ChimichangApp"
-    admin_email: str
-    items_per_user: int = 50
+    app_name: str = "Minimal FastAPI Sample"
+    # Server config
+    api_prefix: str = "/api"
+    version: str
+
+    # PG DB config
+    postgres_user: str
+    postgres_password: str
+    postgres_host: str
+    postgres_port: int
+    postgres_db_name: str
 
     model_config = SettingsConfigDict(env_file="../../.env", extra='ignore')
+
+
--- backend/app/main.py
+++ backend/app/main.py
@@ -25,11 +25,3 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/set")
-async def info(settings: Annotated[config.Settings, Depends(get_settings)]):
-    print(settings)
-    return {
-        "app_name": settings.app_name,
-        "admin_email": settings.admin_email,
-        "items_per_user": settings.items_per_user,
-    }
