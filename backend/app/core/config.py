a8de2061e799558020b52c2e2ed02ddcc5d919f4
Natacha Bertin <32799433+natachabertin@users.noreply.github.com>
2024-01-14 16:37:16 -0300

    config: Settings.db_url calculated property

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,3 +1,6 @@
+from functools import cached_property
+
+from pydantic import computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -20,5 +23,10 @@ class Settings(BaseSettings):
     postgres_db_name: str
 
     model_config = SettingsConfigDict(env_file="../../.env", extra='ignore')
+
+    @computed_field
+    @cached_property
+    def db_url(self) -> int:
+        return f"postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}@{self.postgres_host}:{self.postgres_port}/{self.postgres_db_name}"
 
 
