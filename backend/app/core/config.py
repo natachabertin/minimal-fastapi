59ed951c2a28f853a052fa91852504c762c155a8
Natacha Bertin <32799433+natachabertin@users.noreply.github.com>
2024-01-20 13:57:45 -0300

    config files: added env and db_echo vars

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,10 +5,14 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    app_name: str = "Minimal FastAPI Sample"
     # Server config
+    app_name: str = "Minimal FastAPI Sample"
     api_prefix: str = "/api"
     version: str
+
+    # Environment config
+    environment="dev"
+    db_echo=True
 
     # Server security
     allowed_origins: list = ["*"]
