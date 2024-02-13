9e883ed570a2a8d2d45e61e2a5ada8cd9466d709
Natacha Bertin <32799433+natachabertin@users.noreply.github.com>
2024-02-13 14:35:18 -0300

    test: added more tests (with and without db, with and without router, error/ok/creation, etc)

--- backend/tests/test_sample.py
+++ backend/tests/test_sample.py
@@ -4,12 +4,64 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 
 @pytest.mark.asyncio
-async def test_get_sample(
+async def test_get_root(
+       async_client: AsyncClient,
+       async_session: AsyncSession
+):
+   response = await async_client.get("/")
+
+   assert response.json() ==  {'message': 'Hello World'}
+   assert response.status_code == 200
+
+
+@pytest.mark.asyncio
+async def test_get_all_samples(
        async_client: AsyncClient,
        async_session: AsyncSession
 ):
    response = await async_client.get("/sample")
 
+   assert response.json() ==  [
+        {"id": 1, "name": "Sample Route", "sample_type": "food_sample", "price": 20.05},
+        {"id": 2, "name": "Sample 1", "sample_type": "paint_sample", "price": 10.05},
+    ]
    assert response.status_code == 200
 
-   resp = response.json() is not None
+
+
+@pytest.mark.asyncio
+async def test_post_sample(
+       async_client: AsyncClient,
+       async_session: AsyncSession
+):
+    payload = {
+        "name": "some name",
+        "sample_type": "paint",
+        "price": 10
+    }
+    response = await async_client.post(
+        "/sample", json=payload
+    )
+
+    assert response.json() == {
+        'name': 'some name',
+        'sample_type': 'paint',
+        'price': 10.0
+    }
+    assert response.status_code == 201
+
+
+
+@pytest.mark.asyncio
+async def test_post_sample_error(
+       async_client: AsyncClient,
+       async_session: AsyncSession
+):
+   response = await async_client.post("/sample")
+
+   assert response.json() == {'detail': [{'input': None,
+             'loc': ['body'],
+             'msg': 'Field required',
+             'type': 'missing',
+             'url': 'https://errors.pydantic.dev/2.5/v/missing'}]}
+   assert response.status_code == 422
