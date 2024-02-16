718b3becce3cc27a73317d5f260127c1008ab8f0
natachabertin <natachabertin92@gmail.com>
2024-02-16 12:15:04 -0300

    Conftest: working tests with server and database

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,73 +1,39 @@
-# import asyncio
-# import asyncpg
-# from typing import Generator
-#
-# import pytest, pytest_asyncio
-# from httpx import AsyncClient
-# from sqlalchemy.orm import sessionmaker
-# from sqlmodel import SQLModel
-# from sqlmodel.ext.asyncio.session import AsyncSession
-#
-# from app.core.config import settings
-# from app.db.session import async_engine
-# from app.main import app
-#
-#
-# @pytest.fixture(scope="session")
-# def event_loop(request) -> Generator:  # noqa: indirect usage
-#    loop = asyncio.get_event_loop_policy().new_event_loop()
-#    yield loop
-#    loop.close()
-#
-#
-# async def create_testing_database():
-#     connection_url = "postgresql://postgres:postgres@localhost/postgres_test"
-#     conn = await asyncpg.connect(connection_url)
-#     db_name = settings.postgres_db_name
-#     try:
-#         # Execute SQL command to create the database
-#         await conn.execute("CREATE DATABASE %(db_name)s", db_name)
-#     except asyncpg.exceptions.DuplicateDatabaseError:
-#         print("Database already exists")
-#     finally:
-#         await conn.close()
-#
-#
-# @pytest_asyncio.fixture
-# async def async_client():
-#    async with AsyncClient(
-#            app=app,
-#            base_url="http://127.0.0.1:8000"
-#    ) as client:
-#       yield client
-#
-#
-# @pytest_asyncio.fixture(scope="function")
-# async def async_session() -> AsyncSession:
-#     await create_testing_database()
-#     session = sessionmaker(
-#         async_engine, class_=AsyncSession, expire_on_commit=False
-#     )
-#
-#     async with session() as sess:
-#         async with async_engine.begin() as conn:
-#             await conn.run_sync(SQLModel.metadata.create_all)
-#
-#         yield sess
-#
-#     async with async_engine.begin() as conn:
-#         await conn.run_sync(SQLModel.metadata.drop_all)
-#
-#     await async_engine.dispose()
-
 import asyncio
 import asyncpg
+from typing import Generator
+
+import pytest, pytest_asyncio
+from httpx import AsyncClient
+from sqlalchemy.orm import sessionmaker
+from sqlmodel import SQLModel
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+from app.core.config import settings
+from app.db.session import async_engine
+from app.main import app
+
+
+@pytest.fixture(scope="session")
+def event_loop(request) -> Generator:  # noqa: indirect usage
+   loop = asyncio.get_event_loop_policy().new_event_loop()
+   yield loop
+   loop.close()
+
+
+
 
 async def create_testing_database():
+    """
+    WARN: This is a challenge non prod project; so, in order to create the test db on start,
+    it connects to postgres standard DB (in here, prod db) and creates the test db in the next line.
+
+    If you use this code in a productive CI/CD, you may wanna change the way the DB is created.
+    You may not have permissions to connect to prod DB and create another DB from there.
+    You should go further and create a user with permissions first,
+    or just create a blank DB_test in a previous step in makefile test command.
+    """
     try:
-        # Establish connection to the default database
         conn = await asyncpg.connect("postgresql://postgres:postgres@localhost/postgres")
-        # Execute SQL command to create the testing database
         await conn.execute("CREATE DATABASE postgres_test")
         print("Testing database created successfully")
     except asyncpg.exceptions.DuplicateDatabaseError:
@@ -75,8 +41,30 @@ async def create_testing_database():
     finally:
         await conn.close()
 
-async def main():
-    await create_testing_database()
 
-if __name__ == "__main__":
-    asyncio.run(main())
+@pytest_asyncio.fixture
+async def async_client():
+   async with AsyncClient(
+           app=app,
+           base_url="http://127.0.0.1:8000"
+   ) as client:
+      yield client
+
+
+@pytest_asyncio.fixture(scope="function")
+async def async_session() -> AsyncSession:
+    await create_testing_database()
+    session = sessionmaker(
+        async_engine, class_=AsyncSession, expire_on_commit=False
+    )
+
+    async with session() as sess:
+        async with async_engine.begin() as conn:
+            await conn.run_sync(SQLModel.metadata.create_all)
+
+        yield sess
+
+    async with async_engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.drop_all)
+
+    await async_engine.dispose()
